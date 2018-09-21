94999db74b6c0b6cb6739d7985da66be1161dfd2
Tiange Wang <wtgkevin@gmail.com>
2018-09-21 09:03:27 -0700

    Initial Commit

---
+++ Belt Exam 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Belt Exam 2
+//
+//  Created by Tiange Wang on 9/21/18.
+//  Copyright © 2018 Tiange Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
